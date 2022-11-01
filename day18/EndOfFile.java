9c6ca47c31c3e86ab8ce0f1bdcf28366f940b4a9
satakshikumari <114677623+satakshikumari@users.noreply.github.com>
2022-11-01 23:04:23 +0530

    End of file

---
+++ day18/EndOfFile.java
@@ -0,0 +1,40 @@
+/*
+Input Format
+Read some unknown  lines of input from stdin(System.in) until you reach EOF; each line of input contains a non-empty String.
+
+Output Format
+For each line, print the line number, followed by a single space, and then the line content received as input.
+
+Sample Input
+
+Hello world
+I am a file
+Read me until end-of-file.
+
+
+Code*/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+        int i = 1;
+        while (scan.hasNext()){
+            String s = scan.nextLine();
+            System.out.println(i + " " +s);
+            i++;
+        }
+    }
+
+    }
+
+
+/*Output
+1 Hello world
+2 I am a file
+3 Read me until end-of-file. */
