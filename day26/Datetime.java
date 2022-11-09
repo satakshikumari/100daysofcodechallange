9e9cf47fbebaa397b0ba1aa35afe34bf7135451d
satakshikumari <114677623+satakshikumari@users.noreply.github.com>
2022-11-09 22:40:06 +0530

    Date and Time

---
+++ day26/Datetime.java
@@ -0,0 +1,70 @@
+/* HackerRank Date and Time
+
+Function Description
+
+Complete the findDay function in the editor below.
+
+findDay has the following parameters:
+
+int: month
+int: day
+int: year
+Returns
+
+string: the day of the week in capital letters
+Input Format
+
+A single line of input containing the space separated month, day and year, respectively, in MM DD YY format.
+
+Code /*
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month-1, day);        
+        String arr[] = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
+        return arr[cal.get(Calendar.DAY_OF_WEEK)-1];
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
