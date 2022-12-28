7b5aaa95235071ac289bf3af7a94089befdcd89d
satakshikumari <114677623+satakshikumari@users.noreply.github.com>
2022-12-28 21:49:31 +0530

    Sparse Array (HackerRank)

---
+++ day58/SparseArray.java
@@ -0,0 +1,83 @@
+/* 
+Sparse Array | HackerRank
+
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'matchingStrings' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY stringList
+     *  2. STRING_ARRAY queries
+     */
+
+    public static List<Integer> matchingStrings(List<String> stringList, List<String> queries) {
+    // Write your code here
+    
+    List<Integer> ans=new ArrayList<>();
+    int c=0;
+    for(int i=0;i<queries.size();i++){
+        c=0;
+        for(int j=0;j<stringList.size();j++){
+            if(stringList.get(j).equals(queries.get(i)))
+                c++;
+        }
+        ans.add(c);
+    }
+    
+    return ans;
+    }
+}
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int stringListCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> stringList = new ArrayList<>();
+
+        for (int i = 0; i < stringListCount; i++) {
+            String stringListItem = bufferedReader.readLine();
+            stringList.add(stringListItem);
+        }
+
+        int queriesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> queries = new ArrayList<>();
+
+        for (int i = 0; i < queriesCount; i++) {
+            String queriesItem = bufferedReader.readLine();
+            queries.add(queriesItem);
+        }
+
+        List<Integer> res = Result.matchingStrings(stringList, queries);
+
+        for (int i = 0; i < res.size(); i++) {
+            bufferedWriter.write(String.valueOf(res.get(i)));
+
+            if (i != res.size() - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
