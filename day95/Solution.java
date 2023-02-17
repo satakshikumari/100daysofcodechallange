3b1240325e01743f9575b2fd632212d7d7871ba7
satakshikumari <114677623+satakshikumari@users.noreply.github.com>
2023-02-17 20:40:28 +0530

    RomanToInteger- LeetCode

---
+++ day95/Solution.java
@@ -0,0 +1,42 @@
+/*
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Example 3:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+Code
+*/
+class Solution {
+    public int romanToInt(String s) {
+        Map <Character, Integer> map= new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int result= map.get(s.charAt(s.length()-1));
+        for(int i=s.length()-2;i>=0;i--){
+            if(map.get(s.charAt(i))<map.get(s.charAt(i+1))){
+                result-=map.get(s.charAt(i));
+            }
+            else
+                 result+=map.get(s.charAt(i));
+        }
+        return result;
+    }
+}
