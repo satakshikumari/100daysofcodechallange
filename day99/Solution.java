3192018888ac3eccb667158e0f70db0ffbbb30d1
satakshikumari <114677623+satakshikumari@users.noreply.github.com>
2023-02-21 19:24:01 +0530

    MaximumDepthofBinaryTree- LeetCode

---
+++ day99/Solution.java
@@ -0,0 +1,22 @@
+/*
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+Input: root = [1,null,2]
+Output: 2
+Code-
+*/
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        // Base Condition
+        if(root == null) return 0;
+        // Hypothesis
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        // Induction
+        return Math.max(left, right) + 1;
+    }
+}
